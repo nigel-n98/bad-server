247521b9d6a5ed8edbed72e4928cd9c80a172faa
Najot Nafasov <nigelnafasov98@yandex.com>
2026-03-24 20:38:39 +0500

    error correction for passing autotests v4

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
+import crypto from 'crypto'
+
+import { fileTypeFromBuffer } from 'file-type'
+
 import BadRequestError from '../errors/bad-request-error'
 
 export const uploadFile = async (
@@ -22,9 +26,18 @@ export const uploadFile = async (
             return next(new BadRequestError('Файл слишком большой'))
         }
 
+        const fileType = await fileTypeFromBuffer(req.file.buffer)
+
+        if (!fileType || !fileType.mime.startsWith('image/')) {
+            return next(new BadRequestError('Неверный формат файла'))
+        }
+
+        const ext = `.${fileType.ext}`
+        const filename = `${crypto.randomUUID()}${ext}`
+
         const fileName = process.env.UPLOAD_PATH
-            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
-            : `/${req.file.filename}`
+            ? `/${process.env.UPLOAD_PATH}/${filename}`
+            : `/${filename}`
 
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName,
--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -1,42 +1,8 @@
 import { Request, Express } from 'express'
 import multer, { FileFilterCallback } from 'multer'
-import { mkdirSync } from 'fs'
-import { join, extname } from 'path'
-import uniqueSlug from 'unique-slug'
 import BadRequestError from '../errors/bad-request-error'
 
-type DestinationCallback = (error: Error | null, destination: string) => void
-type FileNameCallback = (error: Error | null, filename: string) => void
-
-const storage = multer.diskStorage({
-    destination: (
-        _req: Request,
-        _file: Express.Multer.File,
-        cb: DestinationCallback
-    ) => {
-        const destinationPath = join(
-            __dirname,
-            process.env.UPLOAD_PATH_TEMP
-                ? `../public/${process.env.UPLOAD_PATH_TEMP}`
-                : '../public'
-        )
-
-        mkdirSync(destinationPath, { recursive: true })
-
-        cb(null, destinationPath)
-    },
-
-    filename: (
-        _req: Request,
-        file: Express.Multer.File,
-        cb: FileNameCallback
-    ) => {
-        const safeExt = extname(file.originalname)
-        const safeName = uniqueSlug()
-
-        cb(null, `${safeName}${safeExt}`)
-    },
-})
+const storage = multer.memoryStorage()
 
 const allowedTypes = [
     'image/png',
